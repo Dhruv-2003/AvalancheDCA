99186979218f83bfb824adc318ba81cfebaa071c
nick-w3 <nick-w3@nick-w3s-MBP.home>
2024-02-19 23:06:59 +0200

    update

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -48,7 +48,7 @@ export default function App({ Component, pageProps }: AppProps) {
   );
 
   const { chains, publicClient } = configureChains(
-    [avalancheFuji, avalanche],
+    [avalancheFuji],
     [publicProvider()]
   );
 
