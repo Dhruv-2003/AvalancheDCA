a27e2189796f6a2b42ec5c043b1de39b74310659
nick-w3 <nick-w3@nick-w3s-MBP.home>
2024-02-20 01:01:05 +0200

    update

--- app/next.config.js
+++ app/next.config.js
@@ -2,24 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
-};
 
-// next.config.js
-module.exports = {
-  ...nextConfig,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env"],
-          plugins: ["@babel/plugin-syntax-dynamic-import"],
-        },
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        fs: false,
+        net: false,
+        tls: false,
       },
-    });
-
+    };
     return config;
   },
 };
+
+module.exports = nextConfig;
